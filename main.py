f1e991d4c7e3d6686f8ddb65c0b0fb1528cbc420
2bruh4me <tg.bruh@protonmail.com>
2020-08-12 19:12:46 +0200

    Add argument parsing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 """ Entry point for the script """
 
+from argparse import ArgumentParser
+
 from config import Config
 
 if __name__ == '__main__':
+    parser = ArgumentParser("Organize files in a directory.")
+    parser.add_argument("directory", metavar="dir", type=str,
+                        help="Directory to organize")
+    args = parser.parse_args()
     config = Config()
+
+    if args.directory:
+        target_dir = args.directory
+        print(target_dir)
