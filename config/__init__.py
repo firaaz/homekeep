b352e1daa60e5ad0f49eb61e196b16e61b8a8778
2bruh4me <tg.bruh@protonmail.com>
2020-08-12 16:14:17 +0200

    Progress on config module

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,23 @@
 """ module for managing configurations """
+
+import os
+from dataclasses import dataclass
+from configparser import ConfigParser
+
+
+@dataclass
+class Config:
+    config_path: str
+
+    def __init__(self):
+        self.config_path = get_config_path()
+        self.config = set_config(config_path)
+
+
+def get_config_path():
+    if (os.environ.get("XDG_CONFIG_HOME"):
+        return os.path.join(os.environ["XDG_CONFIG_HOME"], "homekeep.ini")
+    return os.path.join(os.environ["HOME"], ".config", "homekeep.ini")
+
+def set_config(config_path):
+    config = ConfigParser()
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 """ Entry point for the script """
+
+if __name__ == '__main__':
+    print('homekeep')
