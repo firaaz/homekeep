a65a40b91528177fcef28fa388303d3f6fea8c46
firaaz farook <firaazfarook19@gmail.com>
2020-08-18 16:09:06 +0530

    dev QoL
    added editorconfig

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from handler import Handler
 
 
 def main():
+    """ Main control flow of the program """
+
     parser = ArgumentParser("Organize files in a directory.")
     parser.add_argument("directory", metavar="dir", type=str,
                         help="Directory to organize")
